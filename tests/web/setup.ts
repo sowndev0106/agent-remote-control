533476d3357c293eb07cd620a08b58caf9f04b1a
sown <sowndev0106@gmail.com>
2026-05-28 00:14:52 +0700

    test: add browser shims for web component tests

--- tests/web/setup.ts
+++ tests/web/setup.ts
@@ -1,6 +1,21 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach, vi } from "vitest";
+import { afterEach, beforeEach, vi } from "vitest";
+
+class TestResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+beforeEach(() => {
+  if (typeof ResizeObserver === "undefined") {
+    vi.stubGlobal("ResizeObserver", TestResizeObserver);
+  }
+  if (typeof HTMLElement !== "undefined" && !HTMLElement.prototype.scrollIntoView) {
+    HTMLElement.prototype.scrollIntoView = () => {};
+  }
+});
 
 afterEach(() => {
   if (typeof document !== "undefined") {
